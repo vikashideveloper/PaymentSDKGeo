aaf194328b7df2b561a4a12f048d05196bed8083
Vikash Kumar <biwal.vikash@gmail.com>
2017-10-16 21:51:11 +0530

    Initial Commit

---
+++ GeoPaymentSDK/GeoPaymentSDK.h
@@ -0,0 +1,19 @@
+//
+//  GeoPaymentSDK.h
+//  GeoPaymentSDK
+//
+//  Created by Vikash Kumar on 16/10/17.
+//  Copyright © 2017 Vikash Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoPaymentSDK.
+FOUNDATION_EXPORT double GeoPaymentSDKVersionNumber;
+
+//! Project version string for GeoPaymentSDK.
+FOUNDATION_EXPORT const unsigned char GeoPaymentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoPaymentSDK/PublicHeader.h>
+
+
